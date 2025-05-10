47dd169bb2d7d47d82c3fdcdc7d557263e57b342
Norphel-911 <tenzinnorphel75@gmail.com>
2025-05-10 20:23:41 +0200

    Add files via upload

--- website/views.py
+++ website/views.py
@@ -5,8 +5,8 @@ from flask import Blueprint, render_template
 views = Blueprint('views', __name__)
 
 @views.route('/') #homepage
-def home():
-    return render_template('home.html') 
+def index():
+    return render_template('index.html') 
 
 
 
