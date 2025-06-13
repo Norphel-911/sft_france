85a7065e8dba3b24d2f984be1e67e38b2df5e396
Norphel-911 <tenzinnorphel75@gmail.com>
2025-06-13 07:13:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Add the parent directory of 'website' to sys.path so Python can find it
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from website import create_app
 
 app = create_app()
